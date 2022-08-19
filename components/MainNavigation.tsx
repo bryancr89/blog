e8c44ed2d7392b045f77b1447cdf4cfa8b54d3ca
Bryan Azofeifa <bazofeifa89@gmail.com>
2022-08-19 15:33:04 -0600

    Fix mobile

--- components/MainNavigation.tsx
+++ components/MainNavigation.tsx
@@ -6,8 +6,8 @@ import { ENGLISH, SPANISH } from "../types/global";
 export const MainNavigation = () => {
   const { content, changeLanguage } = useContext(MainContext);
   return (
-    <nav className="sticky top-0 py-3.5 pr-5 flex justify-end overflow-hidden bg-transparent z-10 text-white">
-      <ul className="list-none text-primary flex max-w-8xl justify-end w-1/3">
+    <nav className="py-3.5 pr-5 flex justify-end overflow-hidden bg-transparent z-10 text-white">
+      <ul className="list-none text-primary flex max-w-8xl justify-end sm:w-1/3">
         <li className="mx-1">
           <span className="cursor-pointer" onClick={() => changeLanguage(ENGLISH)}>🇺🇸</span>
           &nbsp; - &nbsp;
